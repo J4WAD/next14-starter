2e776b84fe02ef307d3e1efc4c599f9ca495b200
AbdelDjaouad Azzouz <imjawd@gmail.Com>
2024-12-22 18:57:57 +0100

    a

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,19 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar/Sidebar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: "Djaouad Azzouz",
+  description: "Portfolio",
+  icons: {
+    icon: "/favicon.ico",
+    // You can add more icon types if needed
+    // apple: "/apple-touch-icon.png",
+  },
+  // You can add more metadata fields as needed
+};
 
 export default function RootLayout({ children }) {
   return (
