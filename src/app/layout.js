ba0cbdf2495d0d9fec4f20b06e7737a1e045847b
AbdelDjaouad Azzouz <imjawd@gmail.Com>
2024-12-25 13:01:26 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-arp="">
       <body className={inter.className}>
         <div className="layout">
           <Sidebar />
