c257c719ce251d531c5427388f83ed241071d596
AbdelDjaouad Azzouz <imjawd@gmail.Com>
2025-01-07 21:26:51 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Your existing config options...
+const withPWA = require("next-pwa");
 
-  // Add this to ensure admin files are copied to the build output
+const nextConfig = {
+  reactStrictMode: true,
+
+  // PWA configuration
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+
+  // Webpack configuration
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
     return config;
   },
-};
-// next.config.js
-module.exports = {
+
+  // Rewrites configuration
   async rewrites() {
     return [
       {
@@ -20,14 +28,4 @@ module.exports = {
   },
 };
 
-// next.config.js
-
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
-  pwa: {
-    dest: "public", // This will output the service worker and manifest to the public folder
-    register: true, // Automatically registers the service worker
-    skipWaiting: true, // Skip the waiting phase for the service worker
-  },
-});
+module.exports = withPWA(nextConfig);
